c7d84c5ed31ccf639c8b1b454506a734f9081117
akd420 <ayanakd112@gmail.com>
2023-11-05 14:54:50 +0600

    Travel tips section Done

--- src/Component/Tips.jsx
+++ src/Component/Tips.jsx
@@ -9,16 +9,21 @@ const Tips = () => {
       }}
     >
       <div className="max-w-screen-xl mx-auto mb-12">
-        <h1 className="text-5xl text-center">
-          TRAVEL ESSENTIALS <span className="text-grn">TIPS</span>
-        </h1>
+        <div className="px-2">
+          <h1 className="text-3xl md:text-5xl text-center">
+            TRAVEL TIPS <span className="text-grn">AND ADVICE</span>
+          </h1>
+        </div>
         <div className="flex flex-col justify-center md:flex-row gap-10 py-20 p-4 md:px-5 lg:px-10">
           <div className="md:w-1/2">
             <motion.img
-            whileHover={{
-                rotate:-5,
+              whileHover={{
+                rotate: -5,
                 transition: { duration: 0.5 },
-              }} src="/blog.jpg" alt="" />
+              }}
+              src="/blog.jpg"
+              alt=""
+            />
           </div>
           <div className="md:w-1/2 flex flex-col justify-center">
             <div className="flex gap-6 mb-5">
@@ -37,12 +42,13 @@ const Tips = () => {
               </motion.p>
             </div>
             <h1 className="text-2xl mb-5">
-              MOVING TROUGH: THE EVERYDAY URBAN JUNGLE OF YOUR CITY
+              10 Essential Travel Tips for an Amazing Journey
             </h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididu nt ut labore et dolore magna aliqua. Ut
-              enim. ad minim veniam......{" "}
+              Whether you're a seasoned traveler or just starting your
+              adventure, these travel tips and advice will help you make the
+              most of your journey. From packing essentials to cultural
+              insights, we've got you covered!
             </p>
             <motion.button
               whileHover={{
@@ -50,7 +56,54 @@ const Tips = () => {
                 transition: { duration: 0.1 },
               }}
               whileTap={{ scale: 0.9 }}
-              className="btn bg-grn text-white w-fit mt-5 hover:bg-grn"
+              className="btn bg-grn text-white w-fit mt-5 hover-bg-grn"
+            >
+              Read More
+            </motion.button>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center md:flex-row-reverse gap-10 py-20 p-4 md:px-5 lg:px-10">
+          <div className="md:w-1/2">
+            <motion.img
+              whileHover={{
+                rotate: 5,
+                transition: { duration: 0.5 },
+              }}
+              src="/blog1.jpg"
+              alt=""
+            />
+          </div>
+          <div className="md:w-1/2 flex flex-col justify-center">
+            <div className="flex gap-6 mb-5">
+              <motion.p
+                whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
+              >
+                {" "}
+                <AiOutlineCalendar className="inline-block"></AiOutlineCalendar>{" "}
+                October 10, 2019
+              </motion.p>
+              <motion.p
+                whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
+              >
+                {" "}
+                <GiPencil className="inline-block"></GiPencil> by Ayan Kumar
+              </motion.p>
+            </div>
+            <h1 className="text-2xl mb-5">
+              Explore the World: Top Travel Tips for Adventurers
+            </h1>
+            <p>
+              Embark on a journey to the Peruvian Amazon Rainforest with
+              confidence. Discover the best hiking trails and travel tips for an
+              unforgettable adventure in the heart of nature.
+            </p>
+            <motion.button
+              whileHover={{
+                scale: 1.2,
+                transition: { duration: 0.1 },
+              }}
+              whileTap={{ scale: 0.9 }}
+              className="btn bg-grn text-white w-fit mt-5 hover-bg-grn"
             >
               Read More
             </motion.button>
