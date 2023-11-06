e8e404061081e0770c72d6902ab3960a0e9e3fd2
akd420 <ayanakd112@gmail.com>
2023-11-06 11:38:50 +0600

    added a button to go to top

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,65 @@
+import { useEffect, useState } from "react";
 import Banner from "../Component/Banner";
 import Items from "../Component/Items";
 import LatestBlogs from "../Component/LatestBlogs";
 import NewsLetter from "../Component/NewsLetter";
 import Tips from "../Component/Tips";
+import { motion, useAnimation } from "framer-motion";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const Home = () => {
-    return (
-        <div className=" mx-auto">
-            <Banner></Banner>
-            <LatestBlogs></LatestBlogs>
-            <Tips></Tips>
-            <NewsLetter></NewsLetter>
-            <Items></Items>
-        </div>
-    );
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const controls = useAnimation();
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    if (showScrollButton) {
+      controls.start({ opacity: 1 });
+    } else {
+      controls.start({ opacity: 0 });
+    }
+  }, [showScrollButton, controls]);
+
+  return (
+    <div className=" mx-auto">
+      <Banner></Banner>
+      <LatestBlogs></LatestBlogs>
+      <Tips></Tips>
+      <NewsLetter></NewsLetter>
+      <Items></Items>
+      <motion.button
+        className="fixed bottom-6 right-6 text-2xl text-grn px-4 py-2 rounded-full opacity-0 transition-opacity duration-300 "
+        onClick={scrollToTop}
+        initial={{ opacity: 0 }}
+        animate={controls}
+        whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.1 },
+          }}
+          whileTap={{ scale: 0.9 }}
+      >
+        <BsFillArrowUpCircleFill></BsFillArrowUpCircleFill>
+      </motion.button>
+    </div>
+  );
 };
 
 export default Home;
