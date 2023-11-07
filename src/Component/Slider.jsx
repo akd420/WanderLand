d0394b1c1f7b5dc79c4d65af11f3ff2a9a109b80
akd420 <ayanakd112@gmail.com>
2023-11-07 12:50:29 +0600

    added relevant  text on the slider

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,10 +1,31 @@
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
+const images = [
+  {
+    img: "/slider1.jpg",
+    text: <div className="text-center">
+        <p className="text-lg font-normal">Popular</p>
+        <h1>SWEDEN</h1>
+    </div>,
+  },
+  {
+    img: "/slider2.jpg",
+    text: <div className="text-center">
+    <p className="text-lg font-normal">Popular</p>
+    <h1>STOCKHOLM</h1>
+</div>,
+  },
+  {
+    img: "/slider3.jpg",
+    text: <div className="text-center">
+    <p className="text-lg font-normal">Popular</p>
+    <h1>COLORADO</h1>
+</div>,
+  },
+];
+
 const Slider = () => {
-  const img1 = "/slider1.jpg";
-  const img2 = "/slider2.jpg";
-  const img3 = "/slider3.jpg";
   const [sliderRef] = useKeenSlider(
     {
       loop: true,
@@ -44,9 +65,24 @@ const Slider = () => {
   return (
     <>
       <div ref={sliderRef} className="keen-slider max-w-screen-xl mx-auto h-auto object-cover absolute opacity-75 my-12">
-        <div className="keen-slider__slide number-slide1 mx-auto"><img className="h-[300px] md:h-[500px] lg:h-[800px]" src={img1} alt="" /></div>
-        <div className="keen-slider__slide number-slide1 mx-auto"><img className="h-[300px] md:h-[500px] lg:h-[800px]" src={img2} alt="" /></div>
-        <div className="keen-slider__slide number-slide1 mx-auto"><img className="h-[300px] md:h-[500px] lg:h-[800px]" src={img3} alt="" /></div>
+        {images.map((item, index) => (
+          <div key={index} className="keen-slider__slide number-slide1 mx-auto relative">
+            <div className="h-[300px] md:h-[500px] lg:h-[800px] relative">
+              <img
+                className="h-full w-full object-cover"
+                src={item.img}
+                alt=""
+              />
+              <div
+              style={{
+                backgroundImage: "url(/bg-slider.png)",
+              }}
+              className="absolute bg-no-repeat bg-center top-0 left-0 right-0 bottom-0 flex items-center justify-center text-2xl font-bold opacity-100 transition-opacity duration-300">
+                {item.text}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
